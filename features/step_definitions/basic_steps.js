c00a671d2ac25c36d8a1388cbaec6ac39f12a5b8
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-06 11:55:37 +0100

    adds basic steps to cucumber test, with pending results, adds webpack config with error

---
+++ features/step_definitions/basic_steps.js
@@ -0,0 +1,11 @@
+const { Given, Then } = require("cucumber");
+
+Given("I visit the site", function(){
+
+    return "pending";
+});
+
+Then("I should see {string}", function(string){
+
+    return 'pending';
+});
