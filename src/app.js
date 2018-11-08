857a8c18569238f458391d3e15ffceecaee997e7
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-08 14:50:51 +0100

    adds order to the contacts

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const renderContacts = () => {
     const storage = window.localStorage
     const contacts = JSON.parse(storage.getItem('contacts'))
-    let div = document.querySelector('.contact-list')
+    let div = document.querySelector('.contentt')
 
     if(contacts){
         div.innerHTML = ''
@@ -10,34 +10,40 @@ const renderContacts = () => {
             let li = document.createElement('p')
             if(contact.gender == 'male'){
                 li.innerHTML = `
-                    <div class="content">
-                        <img src = "./images/image1.png" height = "100px" width = "100px"/>
-                        <h1> ${contact.name} </h1>
-                        <h3> ${contact.gender} </h3>
-                        <h2> ${contact.company} </h2>
-                        <p> ${contact.notes} </p>
-                        ${contact.email} |
-                        <a href ="https://www.twitter.com/${contact.twitter}">@${contact.twitter}</a>                
+                <div class:"w-1/4 h-1/4">
+                    <div class="max-w-xs rounded overflow-hidden shadow-lg">
+                        <img class="w-full" src="./images/image1.png" alt="Profile picture">
+                        <div class="px-6 py-4 flex items-center justify-center">
+                            <h1> ${contact.name} </h1>
+                            <h3> ${contact.gender} </h3>
+                            <h2> ${contact.company} </h2>
+                            <p> ${contact.notes} </p>
+                            ${contact.email} |
+                            <a href ="https://www.twitter.com/${contact.twitter}">@${contact.twitter}</a>                
+                        </div>
                     </div>
                 </div>
                 `
             } else if ( contact.gender == "female" ) {
                     li.innerHTML = `
-                    <div class="content">
-                        <img src = "./images/image2.png" height = "100px" width = "100px"/>
-                        <h1> ${contact.name} </h1>
-                        <h3> ${contact.gender} </h3>
-                        <h2> ${contact.company} </h2>
-                        <p> ${contact.notes} </p>
-                        ${contact.email} |
-                        <a href ="https://www.twitter.com/${contact.twitter}">@${contact.twitter}</a>                
+                    <div class:"w-1/4 h-1/4">
+                        <div class="max-w-xs rounded overflow-hidden shadow-lg">
+                            <img class="w-full" src="./images/image2.png" alt="Profile picture">
+                            <div class="px-6 py-4 ">
+                                <h1> ${contact.name} </h1>
+                                <h3> ${contact.gender} </h3>
+                                <h2> ${contact.company} </h2>
+                                <p> ${contact.notes} </p>
+                                ${contact.email} |
+                                <a href ="https://www.twitter.com/${contact.twitter}">@${contact.twitter}</a>                
+                            </div>
+                        </div>
                     </div>
-                </div>
                 `
             }
-            ul.appendChild(li)
+            div.appendChild(li)
         })
-        div.appendChild(ul)
+        //div.appendChild(ul)
     } else{
         div.innerHTML = '<p> You have no contacts in your address book </p>'
     }
